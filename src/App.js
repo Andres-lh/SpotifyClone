bd3f10fa6b00d5a844150934c7da94bde6b1cad4
Andres <andres-lh@outlook.com>
2020-11-22 21:48:10 -0500

    Login system

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from  './Login'
+import { getToken } from './spotify';
 
 function App() {
+  
+  const[token, setToken] =useState(null);
+
+  useEffect( () =>{
+    const hash = getToken();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token){
+      setToken(_token)
+    }
+
+    console.log('I have a token >>>', _token )
+
+  }, [] );
+
   return (
     <div className="app">
-      <h1>Hello</h1>
+      {
+        token ? (<h1>I am logged in</h1>) : (<Login/>)
+      }
+      
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+import { loginUrl } from './spotify';
+
+function Login(){
+    return(
+        <div className="login">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/56/Spotify_logo_horizontal_black.jpg" alt=""/>
+            <a href= {loginUrl}>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
