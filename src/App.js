b37c80443766d17062d44104f8d9f213528fde4f
Andres <andres-lh@outlook.com>
2020-11-24 13:22:11 -0500

    Context Api implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from  './Login'
+import Player from './Player';
 import { getHashFromUrl } from './spotify';
-import SpotifyWebApi from 'spotify-web-api-js'
+import { useDataLayerValue } from './DataLayer';
+import SpotifyWebApi from 'spotify-web-api-js';
+
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   
-  const[token, setToken] =useState(null);
+ 
+  const [{user, token}, dispatch] = useDataLayerValue();
 
   useEffect( () =>{
     const hash = getHashFromUrl();
@@ -16,23 +20,32 @@ function App() {
     const _token = hash.access_token;
 
     if(_token){
-      setToken(_token)
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token
+      })
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user =>{
-        
-        console.log('This is the user '+ user);
+      spotify.getMe().then((user)=>{
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
       })
+
+
     }
 
-    console.log('I have a token >>>', _token );
+    
 
   }, [] );
 
+  console.log('user: ', user)
+  console.log('token: ', token)
   return (
     <div className="app">
       {
-        token ? (<h1>I am logged in</h1>) : (<Login/>)
+        token ? <Player/> : (<Login/>)
       }
       
     </div>
