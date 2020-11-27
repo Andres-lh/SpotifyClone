f21568bea9f2ff6aac78ffc15641cea676a28df3
Andres <andres-lh@outlook.com>
2020-11-26 19:54:53 -0500

    Styling the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar(){
     return(
         <div className="sidebar">
             <img className="sidebar-logo"
-            src="https://comunidadblogger.net/wp-content/uploads/2020/09/spotify-logo.jpg" alt="Logo" />
+            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Logo" />
+            <SidebarOption Icon={HomeIcon} title="Home"/>
+            <SidebarOption Icon={SearchIcon} title="Search"/>
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+            <br/>
+            <strong className="sidebar-title">PLAYLISTS</strong>
+            <hr />
+            <SidebarOption  title="Rock"/>
+            <SidebarOption  title="Hip hop"/>
+
         </div>
         
     )
--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState={
     playlists: [],
     playing: false,
     item: null,
-    token: '',
+    token: 'BQAS9qMnqARUkX40F_JPrIKjAgB9Gdvx8reuRkKxmCxRliBj0PyEqbDgPnzCK19-F_MQXxhIhvjmSW-kIkDLZaIOwVwTJKNfuRlFSZSI9JpEj1pwhYIpQOnwfKCVYZ49Jec2YnlCfVrejqqzSFkGepE-M1xk7GKPu5Va9EIwx37EmWXn',
 
 }
 
