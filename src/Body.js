06435ceac5e5ca810028cddac2382d11328c26a5
Andres <andres-lh@outlook.com>
2020-12-03 13:44:34 -0500

    Body Header

--- src/Body.js
+++ src/Body.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header'
 import './Body.css'
 
-function Body(){
+
+function Body({spotify}){
     return(
         <div className="body">
-            <h1>Body</h1>
+            <Header spotify={spotify}/>
         </div>
         
     )
